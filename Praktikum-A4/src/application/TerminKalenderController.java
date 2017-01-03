f76211d0bd40c1a419ed629eed442d8d6a6edc5f
Alex <alex_horn_93@hotmail.de>
2017-01-04 00:52:55 +0100

    Update

--- Praktikum-A4/src/application/TerminKalenderController.java
+++ Praktikum-A4/src/application/TerminKalenderController.java
@@ -14,102 +14,135 @@ import kalender.*;
 import kalender.TerminMitWiederholungImpl.WiederholungImpl;
 import kalender.interfaces.*;
 
-
 public class TerminKalenderController {
 
-    @FXML
-    private ToggleButton togglebutton;
+	@FXML
+	private ToggleButton togglebutton;
 
-    @FXML
-    private TextField zyklus;
+	@FXML
+	private TextField zyklus;
 
-    @FXML
-    private RadioButton widwoch;
+	@FXML
+	private RadioButton widwoch;
 
-    @FXML
-    private TextField anzahl;
+	@FXML
+	private TextField anzahl;
 
-    @FXML
-    private DatePicker datepick;
+	@FXML
+	private DatePicker datepick;
 
-    @FXML
-    private Button submitbutton;
+	@FXML
+	private Button submitbutton;
 
-    @FXML
-    private TextField dauer;
+	@FXML
+	private TextField dauer;
 
-    @FXML
-    private TextField beschreibung;
+	@FXML
+	private TextField beschreibung;
 
-    @FXML
-    private StackPane stack_pane;
+	@FXML
+	private StackPane stack_pane;
 
-    @FXML
-    private TextField beginn;
+	@FXML
+	private TextField beginn;
 
-    @FXML
-    private RadioButton widtag;
+	@FXML
+	private RadioButton widtag;
 
-    @FXML
-    private TerminKalenderImpl terminkalender = new TerminKalenderImpl();
+	@FXML
+	private TerminKalenderImpl terminkalender = new TerminKalenderImpl();
 
-    @FXML
-    protected void initialize(){
-    	
-    	//Ausschalten der Sachen für Termin
-    	widtag.setVisible(false);
-    	widwoch.setVisible(false);
+	@FXML
+	protected void initialize() {
+
+		// Ausschalten der Sachen für Termin
+		widtag.setVisible(false);
+		widwoch.setVisible(false);
 		zyklus.setVisible(false);
 		anzahl.setVisible(false);
-    	
-    	
-    	//Wenn der Togglebutton gedrückt wird
-    	togglebutton.setOnAction((ActionEvent e) -> {
-    		toggle_effekt();
-    	});
-    	
-    	submitbutton.setOnAction((ActionEvent e) -> {
-    		termin_erstellen();
-    		beginn.clear();
-    		dauer.clear();
-    		beschreibung.clear();
-    	});
-    }
-    
-    @FXML
-    public void toggle_effekt(){
-    	if(togglebutton.isSelected()){
-    		widtag.setVisible(true);
-        	widwoch.setVisible(true);
-    		zyklus.setVisible(true);
-    		anzahl.setVisible(true);
-    	}else{
-    		widtag.setVisible(false);
-        	widwoch.setVisible(false);
-    		zyklus.setVisible(false);
-    		anzahl.setVisible(false);
-    		widtag.setSelected(true);
-    	}
-    	//Damit die Werte beim Switchen nicht erhalten bleiben
-    	zyklus.clear();
-    	anzahl.clear();
-    }
-    
-    
-    @FXML
-    public void termin_erstellen(){
-    	LocalDate locdate = datepick.getValue();
-		TagImpl day = new TagImpl(locdate.getYear(), locdate.getMonthValue()-1, locdate.getDayOfMonth());
+
+		// Wenn der Togglebutton gedrückt wird
+		togglebutton.setOnAction((ActionEvent e) -> {
+			toggle_effekt();
+		});
+
+		submitbutton.setOnAction((ActionEvent e) -> {
+			if (!beginn.getText().isEmpty() && !beschreibung.getText().isEmpty() && !dauer.getText().isEmpty()) {
+				if (!togglebutton.isSelected()) { // Nicht ausgewählt
+					termin_erstellen();
+					field_clear();
+				} else if (!anzahl.getText().isEmpty() && !zyklus.getText().isEmpty()) {
+					termin_erstellen_wid();
+					field_clear();
+				} else {
+
+				}
+
+			}
+		});
+	}
+
+	@FXML
+	public boolean check_field() {
+		if (dauer.getText().matches("\\d+")) {
+			return true;
+		}
+		return false;
+	}
+
+	@FXML
+	public void field_clear() {
+		beginn.clear();
+		dauer.clear();
+		beschreibung.clear();
+		anzahl.clear();
+		zyklus.clear();
+	}
+
+	@FXML
+	public void toggle_effekt() {
+		if (togglebutton.isSelected()) {
+			widtag.setVisible(true);
+			widwoch.setVisible(true);
+			zyklus.setVisible(true);
+			anzahl.setVisible(true);
+		} else {
+			widtag.setVisible(false);
+			widwoch.setVisible(false);
+			zyklus.setVisible(false);
+			anzahl.setVisible(false);
+			widtag.setSelected(true);
+		}
+		// Damit die Werte beim Switchen nicht erhalten bleiben
+		zyklus.clear();
+		anzahl.clear();
+	}
+
+	@FXML
+	public void termin_erstellen() {
+		LocalDate locdate = datepick.getValue();
+		TagImpl day = new TagImpl(locdate.getYear(), locdate.getMonthValue() - 1, locdate.getDayOfMonth());
 		DauerImpl d = new DauerImpl(Integer.valueOf(dauer.getText()));
 		String[] string = beginn.getText().split("-");
-		UhrzeitImpl time = new UhrzeitImpl(Integer.valueOf(string[0]),Integer.valueOf(string[1]));
-		DatumImpl date = new DatumImpl(day,time);
-    	if(togglebutton.isSelected()){		
-    		//Entscheiden ob Täglich oder Wöchentlich ausgewählt ist
-    		String widstring = widtag.isSelected() ? "TAEGLICH" : "WOECHENTLICH";
-    		terminkalender.eintragen(new TerminMitWiederholungImpl(beschreibung.getText(),date,d,WiederholungType.valueOf(widstring),Integer.valueOf(anzahl.getText()),Integer.valueOf(zyklus.getText()) ));
-    	}else{
-    		terminkalender.eintragen(new TerminImpl(beschreibung.getText(),date,d));
-    	}
-    }
+		UhrzeitImpl time = new UhrzeitImpl(Integer.valueOf(string[0]), Integer.valueOf(string[1]));
+		DatumImpl date = new DatumImpl(day, time);
+
+		terminkalender.eintragen(new TerminImpl(beschreibung.getText(), date, d));
+	}
+
+	@FXML
+	public void termin_erstellen_wid() {
+		LocalDate locdate = datepick.getValue();
+		TagImpl day = new TagImpl(locdate.getYear(), locdate.getMonthValue() - 1, locdate.getDayOfMonth());
+		DauerImpl d = new DauerImpl(Integer.valueOf(dauer.getText()));
+		String[] string = beginn.getText().split("-");
+		UhrzeitImpl time = new UhrzeitImpl(Integer.valueOf(string[0]), Integer.valueOf(string[1]));
+		DatumImpl date = new DatumImpl(day, time);
+
+		String widstring = widtag.isSelected() ? "TAEGLICH" : "WOECHENTLICH";
+
+		terminkalender.eintragen(
+				new TerminMitWiederholungImpl(beschreibung.getText(), date, d, WiederholungType.valueOf(widstring),
+						Integer.valueOf(anzahl.getText()), Integer.valueOf(zyklus.getText())));
+	}
 }
